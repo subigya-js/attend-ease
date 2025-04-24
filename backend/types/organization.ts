e8d83c0336817ab03c7b9bc2a93155eace814af0
subigya-js <developer.subigya@gmail.com>
2025-04-24 11:50:09 +0530

    feat: add organization model and type definitions

---
+++ backend/types/organization.ts
@@ -0,0 +1,9 @@
+import { Document, Types } from "mongoose";
+
+export interface OrganizationType extends Document {
+  name: string;
+  orgId: string;
+  admins: Types.ObjectId[];
+  members: Types.ObjectId[];
+  holidays: string[];
+}
